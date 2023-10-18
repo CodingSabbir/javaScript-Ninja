e123de985b6371e5416598b77591a534b936bcc1
MD.Sabbir Rahman <mdsabbirrahmanm740@gmail.com>
2023-10-18 17:07:43 +0600

    event delegation

--- main.js
+++ main.js
@@ -23,20 +23,82 @@ console.log(liElm_2)
 // })
 
 
-const input=document.querySelectorAll('input')
-input.forEach((elm)=>{
-    elm.addEventListener('keyup',(evt)=>{
-       console.log( evt.currentTarget.name)
-       console.log( evt.currentTarget.value)
-    })
-})
+// const input=document.querySelectorAll('input')
+// input.forEach((elm)=>{
+//     elm.addEventListener('keyup',(evt)=>{
+//        console.log( evt.currentTarget.name)
+//        console.log( evt.currentTarget.value)
+//     })
+// })
 
-const h1=document.querySelector('h1')
-h1.addEventListener('mousemove',(evt)=>{
-evt.currentTarget.style.background='pink'
-});
 
-const mouseOut=document.querySelector('h1')
-mouseOut.addEventListener('mouseout',(evt)=>{
-evt.currentTarget.style.background='white'
+
+// const h1=document.querySelector('h1')
+// h1.addEventListener('mousemove',(evt)=>{
+// evt.currentTarget.style.background='pink'
+// });
+
+// const mouseOut=document.querySelector('h1')
+// mouseOut.addEventListener('mouseout',(evt)=>{
+// evt.currentTarget.style.background='white'
+// });
+
+
+
+// const form=document.forms[0]
+// form.addEventListener('submit',(evt)=>{
+//     evt.preventDefault();
+//     const name=document.querySelector('#name')
+//     const email=document.querySelector('#email')
+//     console.log(name.value, email.value)
+// })
+
+
+// document.querySelector('li').addEventListener('click',(evt)=>{
+//     console.log('you have clicked li')
+//     console.log('li',evt.target)
+//     console.log('li',evt.currentTarget)
+//     // evt.stopPropagation()
+// })
+
+// document.querySelector('ul').addEventListener('click',(evt)=>{
+//     console.log('you have clicked UL')
+//     console.log('ul',evt.target)
+//     console.log('ul',evt.currentTarget)
+// })
+
+// document.querySelector('body').addEventListener('click',(evt)=>{
+//     console.log('you have clicked body')
+//     console.log('body',evt.target)
+//     console.log('body',evt.currentTarget)
+// })
+
+
+
+// document.querySelector('li').addEventListener('click',(evt)=>{
+//     console.log('you have clicked li')
+//     console.log('li',evt.target)
+//     console.log('li',evt.currentTarget)
+//     // evt.stopPropagation()
+// },true)
+
+// document.querySelector('ul').addEventListener('click',(evt)=>{
+//     console.log('you have clicked UL')
+//     console.log('ul',evt.target)
+//     console.log('ul',evt.currentTarget)
+// },true)
+
+// document.querySelector('body').addEventListener('click',(evt)=>{
+//     console.log('you have clicked body')
+//     console.log('body',evt.target)
+//     console.log('body',evt.currentTarget)
+// },true)
+
+
+
+const ulElement=document.querySelector('ul')
+ulElement.addEventListener('click',(evt)=>{
+if(evt.target.tagName ==='LI'){
+    console.log('ul',evt.target.textContent)
+}
 });
